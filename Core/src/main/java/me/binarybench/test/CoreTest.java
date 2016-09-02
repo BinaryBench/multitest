df6dd3849104c2d0e2cddbad015486709d98cb5c
Bench <BinaryBench@gmail.com>
2016-09-02 12:20:11 -0400

    Changed tests

--- Core/src/main/java/me/binarybench/test/CoreTest.java
+++ Core/src/main/java/me/binarybench/test/CoreTest.java
@@ -1,5 +1,7 @@
 package me.binarybench.test;
 
+import com.google.common.io.Files;
+import org.apache.commons.dbcp2.BasicDataSource;
 /**
  * Created by Bench on 9/1/2016.
  */
@@ -7,7 +9,9 @@ public class CoreTest {
 
     public static void coolCool()
     {
+        new BasicDataSource();
         System.out.println("Cool cool!");
+        Files.isFile();
     }
 
 }
--- Game/src/main/java/me/binarybench/game/Main.java
+++ Game/src/main/java/me/binarybench/game/Main.java
@@ -1,16 +1,14 @@
 package me.binarybench.game;
 
 import me.binarybench.test.CoreTest;
-import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Created by Bench on 9/1/2016.
  */
-public class Main extends JavaPlugin {
+public class Main {
     public static void main(String[] args)
     {
-
         CoreTest ct = new CoreTest();
-        ct.coolCool();
+        CoreTest.coolCool();
     }
 }
