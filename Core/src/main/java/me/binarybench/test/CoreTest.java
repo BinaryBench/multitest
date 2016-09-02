6c734934d4179bec662830a64b7c0ed0ffab7553
Bench <BinaryBench@gmail.com>
2016-09-02 00:31:20 -0400

    initial commit

---
+++ Core/src/main/java/me/binarybench/test/CoreTest.java
@@ -0,0 +1,13 @@
+package me.binarybench.test;
+
+/**
+ * Created by Bench on 9/1/2016.
+ */
+public class CoreTest {
+
+    public static void coolCool()
+    {
+        System.out.println("Cool cool!");
+    }
+
+}
---
+++ Game/src/main/java/me/binarybench/game/Main.java
@@ -0,0 +1,14 @@
+package me.binarybench.game;
+
+import me.binarybench.test.CoreTest;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Bench on 9/1/2016.
+ */
+public class Main extends JavaPlugin {
+    public static void main(String[] args)
+    {
+        CoreTest.coolCool();
+    }
+}
