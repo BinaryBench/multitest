f9a806c4014778ee0ca72bc8f25f001707c79bd9
Bench <BinaryBench@gmail.com>
2016-09-02 13:23:32 -0400

    Moved stuff

--- Game/src/main/java/me/binarybench/game/Main.java
+++ Game/src/main/java/me/binarybench/game/Main.java
@@ -2,6 +2,7 @@ package me.binarybench.game;
 
 import me.binarybench.test.CoreTest;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.omg.PortableServer.THREAD_POLICY_ID;
 
 /**
  * Created by Bench on 9/1/2016.
@@ -11,5 +12,24 @@ public class Main {
     {
         CoreTest ct = new CoreTest();
         CoreTest.coolCool();
+
+
     }
+
+    public Main()
+    {
+        takeRunnable(this::testMethod);
+    }
+
+    public void testMethod()
+    {
+
+    }
+
+    public void takeRunnable(Runnable runnable)
+    {
+
+    }
+
+
 }
